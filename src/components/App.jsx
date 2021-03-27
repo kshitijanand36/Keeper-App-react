c45ae8d1d09d6a65b78153cde92dc8a4054fd48f
Kshitij Anand <kshitijanand2@gmail.com>
2021-03-27 15:17:13 +0530

    Part 2 Completed!

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,13 +4,29 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+
+function create(props){
+
+    return (
+        <Note
+            key = {props.key} 
+            title = {props.title}
+            content = {props.content}
+
+          />  
+    )
+}
+
 
 function App(){
+
+
 
     return (
         <div>
             <Header />
-            <Note />
+            {notes.map(create)}
             <Footer />
 
         </div>
--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
+import App from './components/App';
 
 ReactDOM.render(
 
